ba014b801053eb28001393a77a5a765ef9ad3f36
Shen Mi <wohenshenmi@gmail.com>
2018-10-13 14:18:40 +0000

    Add account activations

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -28,16 +28,18 @@ class SessionsController extends Controller
        ]);
 
        if (Auth::attempt($credentials, $request->has('remember'))) {
-           session()->flash('success', '欢迎回来！');
-           return redirect()->intended(route('users.show', [Auth::user()]));
+           if(Auth::user()->activated) {
+               session()->flash('success', '欢迎回来！');
+               return redirect()->intended(route('users.show', [Auth::user()]));
+           } else {
+               Auth::logout();
+               session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
+               return redirect('/');
+           }
        } else {
            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
            return redirect()->back();
        }
-        
-        Auth::login($user);
-        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
-        return redirect()->route('users.show', [$user]);
     }
     
     public function destroy()
